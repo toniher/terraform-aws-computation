2d03613bfae851f5456ba56bb9ec56eea2b75a8c
Toni Hermoso Pulido <toniher@cau.cat>
2022-09-20 17:32:59 +0200

    update

---
+++ batch.tf
@@ -0,0 +1,52 @@
+// Batch configuration
+
+resource "aws_batch_compute_environment" "compute" {
+
+  compute_environment_name = format("%s-%s", var.batch_compute_environment_name, random_string.rand.result)
+
+  compute_resources {
+    instance_role = aws_iam_instance_profile.ComputeInstanceProfile.arn
+
+    image_id = var.batch_ami
+
+    max_vcpus     = var.batch_max_vcpus
+    min_vcpus     = var.batch_min_vcpus
+    desired_vcpus = var.batch_desired_vcpus
+
+    instance_type = var.batch_instance_type
+
+    subnets = var.batch_subnets
+
+    type = var.batch_compute_environment_type
+
+    spot_iam_fleet_role = (var.batch_compute_environment_type == "SPOT" ? aws_iam_role.ClusterFleetRole.arn : null)
+
+    bid_percentage = (var.batch_compute_environment_type == "SPOT" ? var.batch_bid_percentage : null)
+
+    security_group_ids = [aws_security_group.allow_all.id]
+
+  }
+
+  service_role = aws_iam_role.ClusterRole.arn
+  type         = "MANAGED"
+  depends_on   = [aws_iam_policy_attachment.AWSBatchServiceRole-policy-attachment]
+
+  tags = {
+    name = format("compute-%s-%s", var.batch_compute_environment_name, random_string.rand.result)
+  }
+}
+
+
+resource "aws_batch_job_queue" "queue" {
+
+  name                 = var.batch_queue_name
+  state                = "ENABLED"
+  priority             = 1
+  compute_environments = [aws_batch_compute_environment.compute.arn]
+
+  depends_on = [aws_batch_compute_environment.compute]
+
+  tags = {
+    name = "queue-${var.batch_queue_name}"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -53,3 +53,53 @@ variable "repo_url" {
   type    = string
   default = ""
 }
+
+// Batch variables
+
+variable "batch_subnets" {
+  type    = list(string)
+  default = ["subnet-8a280df7", "subnet-c54d6588", "subnet-b85ab5d2"]
+}
+variable "batch_ami" {
+  type = string
+}
+
+variable "batch_bid_percentage" {
+  type    = number
+  default = 50
+}
+
+variable "batch_max_vcpus" {
+  type    = number
+  default = 16
+}
+
+variable "batch_min_vcpus" {
+  type    = number
+  default = 0
+}
+
+variable "batch_desired_vcpus" {
+  type    = number
+  default = 0
+}
+
+variable "batch_instance_type" {
+  type    = list(string)
+  default = ["optimal"]
+}
+
+variable "batch_compute_environment_name" {
+  type    = string
+  default = "nf-compute-spot"
+}
+
+variable "batch_compute_environment_type" {
+  type    = string
+  default = "SPOT"
+}
+
+variable "batch_queue_name" {
+  type    = string
+  default = "spot"
+}
